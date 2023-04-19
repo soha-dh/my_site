cb561393093afcf2f634dfab567d0c7505de70a5
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2023-04-19 15:34:54 +0330

    55 enter Dumy Data for test on Views

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,49 @@
+from datetime import date
 from django.shortcuts import render
+
+posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "sohastar",
+        "date": date(2021, 7, 21),
+        "title": "Mounting Hiking",
+        "excerpt": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio sequi quasi nisi rerum nemo perspiciatis nihil tenetur doloremque dolorum amet!",
+        "content": """ 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut corporis nostrum sequi 
+            accusantium facere reiciendis dignissimos temporibus, in architecto fugit eaque, 
+            molestias quisquam nisi consectetur deserunt ut ipsum perspiciatis hic.
+            """
+
+    },  {
+        "slug": "swiming-in the sea",
+        "image": "mountains.jpg",
+        "author": "elham",
+        "date": date(2021, 8, 22),
+        "title": "Mounting Hiking",
+        "excerpt": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio sequi quasi nisi rerum nemo perspiciatis nihil tenetur doloremque dolorum amet!",
+        "content": """ 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut corporis nostrum sequi 
+            accusantium facere reiciendis dignissimos temporibus, in architecto fugit eaque, 
+            molestias quisquam nisi consectetur deserunt ut ipsum perspiciatis hic.
+            """
+
+    }, {
+        "slug": "Dansing-in-the-club",
+        "image": "mountains.jpg",
+        "author": "mahsa",
+        "date": date(2021, 9, 23),
+        "title": "Mounting Hiking",
+        "excerpt": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio sequi quasi nisi rerum nemo perspiciatis nihil tenetur doloremque dolorum amet!",
+        "content": """ 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut corporis nostrum sequi 
+            accusantium facere reiciendis dignissimos temporibus, in architecto fugit eaque, 
+            molestias quisquam nisi consectetur deserunt ut ipsum perspiciatis hic.
+            """
+
+    }
+]
+
 
 # Create your views here.
 
